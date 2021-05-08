b7b3b18b3b6cc4700cc9d88a231f910bec20530b
Aoudheus <77090966+Aoudheus@users.noreply.github.com>
2021-05-08 17:15:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,10 @@ const cadeau = document.querySelector("img");
 const text = document.querySelector("h1");
 const shadow = document.querySelector(".shadow");
 
-cadeau.addEventListener("mouseenter", function () {
-  cadeau.classList.remove("jumping");
-  shadow.classList.add("hidden");
-});
-
-cadeau.addEventListener("mouseleave", function () {
-  cadeau.classList.add("jumping");
-  shadow.classList.remove("hidden");
-});
-
 cadeau.addEventListener("click", function () {
-  cadeau.classList.toggle("active");
-  cadeau.classList.toggle("jumping");
+  cadeau.classList.add("active");
+  cadeau.classList.remove("jumping");
   setTimeout(function () {
-    text.classList.toggle("hidden");
+    text.classList.remove("hidden");
   }, 500);
 });
